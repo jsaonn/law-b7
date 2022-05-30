56d35e45095742f3e38286980c1def919190763d
tsanaativa <tsanaativa.vinnera@ui.ac.id>
2022-05-30 09:53:05 +0700

    fix user data

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 import Main from "./layouts/main/Main";
 import { useState } from 'react';
 import { UserContext } from './pages/auth/UserContext';
-import jwt_decode from "jwt-decode";
 
 function App() {
-  let [user, setUser] = useState(()=> localStorage.getItem('access_token') ? jwt_decode(localStorage.getItem('access_token')) : null);
+  let [user, setUser] = useState(()=> localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null);
   return (
     <Router>
       <UserContext.Provider value={{user, setUser}}>
